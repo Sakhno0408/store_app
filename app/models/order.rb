a41578f6313d290f319b5e542979748d999d7c55
sakhno0408 <sakhno0408@gmail.com>
2021-01-19 19:32:48 +0200

    recommented validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,12 +4,11 @@ class Order < ApplicationRecord
   has_many :products, through: :order_items
   belongs_to :user
 
-  # validates :address, :credit_card, presence: true
-  # validates :credit_card, numericality: true
-  #
-  #
-  # validates :credit_card, length: { minimum: 5 }
-  # validates :credit_card, length: { is: 16 }
+  validates :address, :credit_card, presence: true
+  validates :credit_card, numericality: true
+  
+  validates :credit_card, length: { minimum: 5 }
+  validates :credit_card, length: { is: 16 }
 
 
 end
